03f4d6b73baa6dbbc9e037073a84c542cb104349
Kammachi <126817246+Kammachi@users.noreply.github.com>
2024-06-03 21:30:59 +0200

    Create website design

--- script.js
+++ script.js
@@ -15,11 +15,14 @@ function addBooktoLibrary(bookToAdd) {
 const book1 = new Book("Title1", "Author1", 0, 2000, true);
 const book2 = new Book("Title2", "Author2", 122, 1992, false);
 const book3 = new Book("Title3", "Author3", 900, 1094, true);
+const book4 = new Book("The Brothers Karamazov", "Fyodor Dostoevsky", 
+960, 1880, true);
 
 
 addBooktoLibrary(book1);
 addBooktoLibrary(book2);
 addBooktoLibrary(book3);
+addBooktoLibrary(book4);
 
 
 function displayLibrary() {
@@ -28,9 +31,17 @@ function displayLibrary() {
     for (let book of myLibrary) {
         let row = document.createElement('tr');
 
-        for (let value of Object.values(book)) {
-            let bookInfo = document.createElement('td');
-            bookInfo.textContent = value;
+        for (let key of Object.keys(book)) {
+            let bookInfo;
+
+            if (key == "title")  {
+                bookInfo = document.createElement('th');
+                bookInfo.setAttribute('scope', 'row')
+            } else {
+                bookInfo = document.createElement('td');
+            }
+
+            bookInfo.textContent = book[key];
 
 
             row.appendChild(bookInfo);
