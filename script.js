5e4a3b9861eb45c6572597d52935d9998194ce36
Kammachi <126817246+Kammachi@users.noreply.github.com>
2024-06-07 22:16:39 +0200

    Create form for adding books

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
 const myLibrary = [];
 
-function Book(title, author, pages, year, read) {
+function Book(title, author, pages, read, rating) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.year = year;
     this.read = read;
+    this.rating = rating;
 }
 
-function addBooktoLibrary(bookToAdd) {
+function addBooktoLibrary(title, author, pages, read, rating) {
+    let bookToAdd = new Book(title, author, pages, read, rating)
     myLibrary.push(bookToAdd);
 }
 
-const book1 = new Book("Title1", "Author1", 0, 2000, true);
-const book2 = new Book("Title2", "Author2", 122, 1992, false);
-const book3 = new Book("Title3", "Author3", 900, 1094, true);
-const book4 = new Book("The Brothers Karamazov", "Fyodor Dostoevsky", 
-960, 1880, true);
+
+addBooktoLibrary("Title1", "Author1", 0, true, 8);
+addBooktoLibrary("Title2", "Author2", 122, false, "-");
+addBooktoLibrary("Title3", "Author3", 900, true, 3);
+addBooktoLibrary("The Brothers Karamazov", "Fyodor Dostoevsky", 
+                960, true, 9);
 
 
-addBooktoLibrary(book1);
-addBooktoLibrary(book2);
-addBooktoLibrary(book3);
-addBooktoLibrary(book4);
+const tableBody = document.querySelector('tbody')
 
 
 function displayLibrary() {
-    const tableBody = document.querySelector('tbody')
 
     for (let book of myLibrary) {
         let row = document.createElement('tr');
@@ -50,5 +48,48 @@ function displayLibrary() {
         tableBody.appendChild(row);
     }
 }
+
+
+const newBook = document.querySelector('.newBookButton');
+const dialog = document.querySelector('#dialog');
+
+newBook.addEventListener('click', () => {
+    dialog.show();
+});
+
+
+
+function discardTable() {
+    for (let book of myLibrary) {
+        tableBody.removeChild(document.querySelector('tbody tr'));
+    }
+}
+
+
+const form = document.querySelector('form');
+const cancel = document.querySelector('#cancel');
+
+form.addEventListener('submit', (event) => {
+
+    const newTitle = document.querySelector('#title');
+    const newAuthor = document.querySelector('#author');
+    const newPages = document.querySelector('#pages');
+    const newRead = document.querySelector('input[name="read"]:checked');
+    const newRating = document.querySelector('#rating');
+
+    discardTable();
+
+    addBooktoLibrary(newTitle.value, newAuthor.value, newPages.value, 
+                    newRead.value, newRating.value)
+
+    displayLibrary();
+
+    event.preventDefault();
+});
+
+cancel.addEventListener('click', () => {
+    dialog.close();
+});
+
 
 displayLibrary();
