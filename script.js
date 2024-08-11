7131c42f0fd6ac23e33669fef4cea4922fb3f715
Kammachi <126817246+Kammachi@users.noreply.github.com>
2024-08-11 17:13:37 +0200

    Refactor constructors into classes

--- script.js
+++ script.js
@@ -2,38 +2,45 @@ const tableBody = document.querySelector('tbody');
 
 const myLibrary = [];
 
-function Book(title, author, pages, read, rating) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.rating = rating;
+class Book {
+    constructor(title, author, pages, read, rating) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.rating = rating;
+    }
+
+    toggleRead() {
+        this.read = this.read === "Yes" ? "No" : "Yes";
+    }
 }
 
-function addBooktoLibrary(title, author, pages, read, rating) {
+
+function addBook(title, author, pages, read, rating) {
     let bookToAdd = new Book(title, author, pages, read, rating)
     myLibrary.push(bookToAdd);
 }
 
-
-addBooktoLibrary("Title2", "Author2", 122, "No", "-");
-addBooktoLibrary("Title1", "Author1", 0, "Yes", 8);
-addBooktoLibrary("Title3", "Author3", 900, "Yes", 3);
-addBooktoLibrary("The Brothers Karamazov", "Fyodor Dostoevsky", 
+// Examples
+addBook("Title2", "Author2", 122, "No", "-");
+addBook("Title1", "Author1", 0, "Yes", 8);
+addBook("Title3", "Author3", 900, "Yes", 3);
+addBook("The Brothers Karamazov", "Fyodor Dostoevsky", 
                 960, "Yes", 9);
 
 
-function displayLibrary() {
-    /* setting a counter to associate buttons with books */
+function renderTable() {
+    // Set a counter to associate buttons with books
     let count = 0;
 
-    /* generating rows for the table */
+    // Generate rows for the table
     for (let book of myLibrary) {
         const row = document.createElement('tr');
         const deleteButton = document.createElement('button');
         const toggleButton = document.createElement('button');
 
-        /* creating an svg element and adding it to the button */
+        // Create an svg element and add it to the button
         const svg = `<svg xmlns="http://www.w3.org/2000/svg"  width="24" 
                     height="24"  viewBox="0 0 24 24"  fill="#663526"  
                     stroke="#EEE"  stroke-width="2"  
@@ -54,20 +61,15 @@ function displayLibrary() {
         toggleButton.setAttribute("data-id", count);
         count++;
 
-        /* generating a row */
+        // Generate a row
         for (const key of Object.keys(book)) {
             let bookInfo;
 
-            if (key === "title")  {
-                bookInfo = document.createElement('th');
-                bookInfo.setAttribute('scope', 'row')
-            } else {
-                bookInfo = document.createElement('td');
-            }
-
+            // Create a cell for each category
+            bookInfo = document.createElement('td');
             bookInfo.textContent = book[key];
 
-            /* adding a toggle button between data */
+            // Add a toggle button
             if (key === "read") {
                 bookInfo.appendChild(toggleButton);
             }
@@ -80,7 +82,7 @@ function displayLibrary() {
         tableBody.appendChild(row);
     }
 
-    /* delete buttons listeners */
+    // Delete buttons listeners
     let deleteButtons = document.querySelectorAll('.deleteButton');
 
     deleteButtons.forEach((button) => {
@@ -94,27 +96,27 @@ function displayLibrary() {
             
 
             discardTable();
-            displayLibrary();
+            renderTable();
         });
     });
 
-    /* toggle buttons listeners */
+    // Toggle buttons listeners
     let toggleButtons = document.querySelectorAll('.toggleButton');
 
     toggleButtons.forEach((button) => {
         button.addEventListener('click', () => {
             let id = parseInt(button.getAttribute('data-id'));
 
-            myLibrary[id].read = myLibrary[id].read === "Yes" ? "No" : "Yes";
+            myLibrary[id].toggleRead();
 
             discardTable()
-            displayLibrary();
+            renderTable();
         });
     });
 }
 
 
-displayLibrary();
+renderTable();
 
 
 function discardTable() {
@@ -123,7 +125,7 @@ function discardTable() {
     }
 }
 
-/* sorting */
+/* Sorting by headers */
 
 const headers = document.querySelectorAll('thead th');
 
@@ -136,6 +138,7 @@ headers.forEach((header) => {
     });
 });
 
+// 
 let criteriaList = {
     "title": false,
     "author": false,
@@ -146,10 +149,13 @@ let criteriaList = {
 
 function sortTable(criteria) {
 
+    // Sorting library based on table header user clicked
     myLibrary.sort((book1, book2) => {
+        // Comparing values of different books and sorting them
         let criteria1 = book1[criteria] === "-" ? "0" : book1[criteria];
         let criteria2 = book2[criteria] === "-" ? "0" : book2[criteria];
 
+        // Statement which determines sorting direction
         let statement = criteriaList[criteria] === true ?
                  criteria1 > criteria2 : criteria1 < criteria2;
 
@@ -162,23 +168,21 @@ function sortTable(criteria) {
         return 1;
     });
 
-    criteriaList[criteria] = !criteriaList[criteria];
+    criteriaList[criteria] = !criteriaList[criteria]
 
     discardTable()
-    displayLibrary();
+    renderTable();
 }
 
 
-
-/* form */
-
+/* Adding a new book */
 
 const newBook = document.querySelector('.newBookButton');
 const dialog = document.querySelector('#dialog');
 const form = document.querySelector('form');
 const cancel = document.querySelector('#cancel');
 
-/* opens the form */
+// Opens a form
 newBook.addEventListener('click', () => {
     dialog.show();
 });
@@ -194,19 +198,20 @@ form.addEventListener('submit', (event) => {
 
     discardTable();
 
-    addBooktoLibrary(newTitle.value, 
+    addBook(newTitle.value, 
             !newAuthor.value ? "-" :newAuthor.value,
             !newPages.value  ? "-" :newPages.value, 
             newRead.value, 
             !newRating.value ? "-" :newRating.value)
 
    
-    displayLibrary();
+    renderTable();
 
+    // Doesn't submit the form
     event.preventDefault();
 });
 
-/* closes the form */
+// Closes a form
 cancel.addEventListener('click', () => {
     dialog.close();
 });
